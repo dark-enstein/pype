50cc5399f5a6a869d447cb4a04aba093ba24a8d7
Ayobami Bamigboye <ayobamibamigboye@Ayobamis-MacBook-Pro.local>
2022-12-25 00:06:09 +0100

    added requirements.txt

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 
 echo "This is built ideally for a cicd tooling where you can ensure that the python version installed"
 profile=~/.pype_profile
+
+echo "Installing python dependencies"
+pip3 install -r requirements.txt
+
 init_pype() {
     if grep -q "alias pype='python3 $(pwd)/pype.py'" $profile; then
       echo pype already installed
