27bb30817f4e6b86257ecf5be039d43ffe09b826
Ayobami Bamigboye <ayobamibamigboye@Ayobamis-MacBook-Pro.local>
2022-12-25 06:10:05 +0100

    fixes

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -7,7 +7,7 @@ echo "------------------------- PYPE TESTING BEGINS -------------------------"
 alias pype='python3 $(pwd)/pype.py'
 rc=/tmp/rcii
 #echo 'shopt -s expand_aliases' > $rc
-echo 'alias pype="python3 pype.py" ' >> $rc
+echo 'alias pype='python3 pype.py' ' >> $rc
 . /tmp/rcii
 #---
 
@@ -19,6 +19,7 @@ else
   echo 1. Setup script run correctly: Pass
 fi
 
+# shellcheck disable=SC2263
 result_not=$(alias pype='python3 pype.py'; pype request; echo $?)
 result=$(pype requests; echo $?)
 
