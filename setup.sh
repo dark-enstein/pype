04ff910a2e4b41e4bddd99b3015bbd7798062496
Ayobami Bamigboye <ayobamibamigboye@Ayobamis-MacBook-Pro.local>
2022-12-25 06:40:23 +0100

    test

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 echo "This is built ideally for a cicd tooling where you can ensure that the python version installed"
 sudo apt update
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 
 profile=~/.pype_profile
 
